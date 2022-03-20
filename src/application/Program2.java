2064cbf5756f18c11fa519da0f1ebd9193c88dfb
EdsonCampolina <ejcsengsoft@gmail.com>
2022-03-20 12:17:18 -0300

    Comentando testes já realizados com sucesso

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -10,19 +10,19 @@ public class Program2 {
 
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-		System.out.println("=== TEST 1: Department insert ===");
-		Department dep = new Department(null, "Test");
-		departmentDao.insert(dep);
-		System.out.println("Inserted! New id = " + dep.getId());
-
-		System.out.println("=== TEST 2: Department update ===");
-		dep.setName("Testing");
-		departmentDao.update(dep);
-		System.out.println("Update completed!");
-
-		System.out.println("=== TEST 3: Department delete ===");
-		departmentDao.deleteById(11);
-		System.out.println("Delete completed!");
+//		System.out.println("=== TEST 1: Department insert ===");
+//		Department dep = new Department(null, "Test");
+//		departmentDao.insert(dep);
+//		System.out.println("Inserted! New id = " + dep.getId());
+//
+//		System.out.println("=== TEST 2: Department update ===");
+//		dep.setName("Testing");
+//		departmentDao.update(dep);
+//		System.out.println("Update completed!");
+//
+//		System.out.println("=== TEST 3: Department delete ===");
+//		departmentDao.deleteById(11);
+//		System.out.println("Delete completed!");
 
 		System.out.println("=== TEST 4: Department findById ===");
 		Department dep = departmentDao.findById(6);
